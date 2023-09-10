9072c2d7a699b3d26752e107976b984902b31f35
Furkan Cingöz <efcingoz@outlook.com>
2023-09-10 18:25:58 +0300

    table view'a aileyi tanımladık

--- simpsonBook/ViewController.swift
+++ simpsonBook/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var simpsonss = [Simpson]()
+    
+    @IBOutlet weak var tabelView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tabelView.dataSource = self
+        tabelView.delegate = self
+        
+        //simpson
+       let homer = Simpson(nameInput: "Homer", jobInput: "Nuclear Safety", imageInput: UIImage(named: "homer")!)
+        let marge = Simpson(nameInput: "Marge", jobInput: "House Wife", imageInput: UIImage(named: "marge")!)
+        let bart = Simpson(nameInput: "Bart", jobInput: "Student", imageInput: UIImage(named: "bart")!)
+        let lisa = Simpson(nameInput: "Lisa", jobInput: "Student", imageInput: UIImage(named: "lisa")!)
+        let maggie = Simpson(nameInput: "Maggie", jobInput: "babby", imageInput: UIImage(named: "maggie")!)
+      
+        simpsonss.append(homer)
+        simpsonss.append(marge)
+        simpsonss.append(bart)
+        simpsonss.append(lisa)
+        simpsonss.append(maggie)
+
+        
+        
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return simpsonss.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = simpsonss[indexPath.row].name
+        cell.contentConfiguration = content
+                return cell
+    }
 }
 
---
+++ simpsonBook/toDetailsVC.swift
@@ -0,0 +1,32 @@
+//
+//  toDetailsVC.swift
+//  simpsonBook
+//
+//  Created by Furkan Cingöz on 10.09.2023.
+//
+
+import UIKit
+
+class toDetailsVC: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var jobLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
