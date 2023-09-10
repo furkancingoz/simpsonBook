ed2496adf075600e5b9dc4406e11ffc386a3e8d0
Furkan Cingöz <efcingoz@outlook.com>
2023-09-10 18:30:03 +0300

    amk dosyası neden eklenmedi

---
+++ simpsonBook/simpson.swift
@@ -0,0 +1,28 @@
+//
+//  simpson.swift
+//  simpsonBook
+//
+//  Created by Furkan Cingöz on 10.09.2023.
+//
+
+import Foundation
+import UIKit
+
+
+class Simpson{
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    
+    init(nameInput: String, jobInput: String, imageInput: UIImage) {
+        name = nameInput
+        job = jobInput
+        image = imageInput
+    }
+    
+    
+    
+    
+}
